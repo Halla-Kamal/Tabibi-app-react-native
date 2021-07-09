a578b402a88cbe85ccdea100e16d5ce5b5329576
Halla_Sidan <h.saidan96@gmail.com>
2021-07-09 18:07:56 +0300

    first commit

---
+++ config/urls.js
@@ -0,0 +1,6 @@
+export const API_URL = 'http://46.32.121.109:5000';
+export const SIGNUP_URL = '/account/signup';
+export const SIGNIN_URL = '/account/signin';
+export const ME_URL = '/account/me';
+export const PROFILE_URL = '/account/profile';
+export const DOCTORS_URL = '/doctors';
